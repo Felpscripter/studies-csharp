0ec80cb0c98b5f63a7bfdecdf2a32b80efc8a444
Felipe Ribeiro <felpscripter@gmail.com>
2026-01-23 17:02:31 -0300

    Feat: Learning Switch.

---
+++ exercise15/Program.cs
@@ -0,0 +1,20 @@
+﻿int mainmenu = 2;
+
+switch (mainmenu)
+{
+    case 1:
+    Console.WriteLine("New game");
+    break;
+
+    case 2:
+    Console.WriteLine("Load game");
+    break;
+
+    case 3:
+    Console.WriteLine("Save game");
+    break;
+
+    default:
+    Console.WriteLine("Invalid Option");
+    break;
+}
