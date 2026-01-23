ea632f74cba6a2011e4983d7d856471928fb12b6
Felipe Ribeiro <felpscripter@gmail.com>
2026-01-23 13:45:54 -0300

    more exercises with int and string.

---
+++ exercise08/Program.cs
@@ -0,0 +1,8 @@
+﻿string name = "Felipe";
+String country = "Brazil";
+
+Console.WriteLine($"My name is {name} e moro no {country}");
+
+int age = 21;
+
+Console.WriteLine($"My name is {name}, moro no {country} and I'm {age}.");
