844505649934d1db0b3bda150aca845d9dc1417e
Felipe Ribeiro <felpscripter@gmail.com>
2026-01-23 13:21:53 -0300

    Feat: Learning char.

---
+++ exercise07/Program.cs
@@ -0,0 +1,14 @@
+﻿char letter = 'A';
+char symbol = '#';
+char number = '1';
+
+
+Console.WriteLine(letter);
+Console.WriteLine(symbol);
+Console.WriteLine(number);
+
+
+char letter2 = 'X';
+letter2 = 'Y';
+
+Console.WriteLine(letter2);
