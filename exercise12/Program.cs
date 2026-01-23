12cb149e8b5458ecfb055d33c5b2c3b04e3fe5fa
Felipe Ribeiro <felpscripter@gmail.com>
2026-01-23 15:10:52 -0300

    Feat: operators 3

---
+++ exercise12/Program.cs
@@ -0,0 +1,13 @@
+﻿int num = 10;
+int num2 = 10;
+int num3 = 11;
+int num6 = 5;
+int num8 = 2;
+
+Console.WriteLine(num == num2);
+Console.WriteLine(num != num3);
+Console.WriteLine(num6 > num8);
+Console.WriteLine(num < num3);
+Console.WriteLine(num >= num2);
+Console.WriteLine(num <= num2);
+Console.WriteLine(num < num8);
