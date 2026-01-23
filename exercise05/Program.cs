7261dee9427c351a746de30894e34b5e1fc4fead
Felipe Ribeiro <felpscripter@gmail.com>
2026-01-23 11:13:41 -0300

    Feat: exercises with double.

---
+++ exercise05/Program.cs
@@ -0,0 +1,20 @@
+﻿double number01 = 10.0;
+double number02 = 4.0;
+
+double result01 = number01 + number02;
+Console.WriteLine(result01);
+
+double result02 = number02 + number01;
+Console.WriteLine(result02);
+
+double result03 = number02 + number01;
+Console.WriteLine(result03);
+
+double result04 = number02 + number01;
+Console.WriteLine(result04);
+
+int numberInt01 = 40;
+
+double result05 = numberInt01 / number02;
+Console.WriteLine(result05);
+
