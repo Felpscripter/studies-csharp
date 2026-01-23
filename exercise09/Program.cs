0dba3d5739c6d6e8e4df439f546594388f0a64e6
Felipe Ribeiro <felpscripter@gmail.com>
2026-01-23 14:00:48 -0300

    Feat: learning var.

---
+++ exercise09/Program.cs
@@ -0,0 +1,21 @@
+﻿var test = 1;
+test = 9;
+
+Console.WriteLine(test);
+
+var test2 = "Hello!";
+
+Console.WriteLine(test2);
+
+var test3 = true;
+
+test3 = false;
+
+Console.WriteLine(test3);
+
+var test4 = 2.5;
+var test5 = 2;
+
+var result = test4 / test5;
+
+Console.WriteLine(result);
