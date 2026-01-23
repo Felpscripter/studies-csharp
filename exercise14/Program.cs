1d8079d3a13b792499109c6f859d4ea413d6353e
Felipe Ribeiro <felpscripter@gmail.com>
2026-01-23 16:12:57 -0300

    Feat: If, else if, else.

---
+++ exercise14/Program.cs
@@ -0,0 +1,27 @@
+﻿using System.Drawing;
+
+int age = 15;
+
+if (age >=18)
+{
+    Console.WriteLine("Of legal age.");
+}
+else
+{
+    Console.WriteLine("Minor.");
+}
+
+float points = 6.9f;
+
+if (points >=7)
+{
+    Console.WriteLine("Green!");
+}
+else if (points <= 6 && points >= 5)
+{
+    Console.WriteLine("Blue!");
+}
+else
+{
+    Console.WriteLine("Red!"); 
+}
