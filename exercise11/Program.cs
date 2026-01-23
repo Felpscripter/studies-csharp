544939fc73b04289d59750a6dbdaa7c60a5fcd5f
Felipe Ribeiro <felpscripter@gmail.com>
2026-01-23 14:28:53 -0300

    Feat: Operators 2

---
+++ exercise11/Program.cs
@@ -0,0 +1,20 @@
+﻿int num = 10;
+num += 5;
+
+Console.WriteLine(num);
+
+int num2 = 15;
+
+num2 -= 5;
+
+Console.WriteLine(num2);
+
+int num3 = 20;
+num3 *= 3;
+
+Console.WriteLine(num3);
+
+int num4 = 25;
+num4 /= 5;
+
+Console.WriteLine(num4);
