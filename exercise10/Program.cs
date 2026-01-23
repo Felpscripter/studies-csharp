afe4fb3fc73cb24ee3677d96cea14f45ad04c0b9
Felipe Ribeiro <felpscripter@gmail.com>
2026-01-23 14:13:56 -0300

    Feat: operators.

---
+++ exercise10/Program.cs
@@ -0,0 +1,27 @@
+﻿int num1 = 1;
+int num2 = 5;
+int num3 = 7;
+int num4 = 10;
+int num5 = 15;
+int num6 = 2;
+
+
+int sum = num1 + num5;
+
+Console.WriteLine(sum);
+
+int sub = num4 + num2;
+
+Console.WriteLine(sub);
+
+int multi = num3 * num4;
+
+Console.WriteLine(multi);
+
+int div = num5 / num1;
+
+Console.WriteLine(div);
+
+int div2 = num4 / num6;
+
+Console.WriteLine(div2);
