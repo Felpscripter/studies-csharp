32507f4ecca708b18295e5843c4e7256f90b2334
Felipe Ribeiro <felpscripter@gmail.com>
2026-01-23 10:41:31 -0300

    Feat: some exercises with int

---
+++ exercise04/Program.cs
@@ -0,0 +1,20 @@
+﻿int number01 = 10;
+int number02 = 90;
+
+int result = number01 + number02;
+Console.WriteLine(result);
+
+int result02 = number02 - number01;
+Console.WriteLine(result02);
+
+int result03 = number01 * number02;
+Console.WriteLine(result03);
+
+int result04 = number02 / number01;
+Console.WriteLine(result04);
+
+int count = 0;
+count = count + 1;
+count = count + 1;
+count = count + 1;
+Console.WriteLine(count);
