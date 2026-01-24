1e32a3955e520f37fea0c3ddd910fbede35441d7
Felipe Ribeiro <felpscripter@gmail.com>
2026-01-24 07:54:51 -0300

    Learning ReadLine().

---
+++ exercise16/Program.cs
@@ -0,0 +1,26 @@
+﻿Console.WriteLine("What is your name?");
+string name = Console.ReadLine()!;
+
+Console.WriteLine("What was your grade?");
+int grade = int.Parse(Console.ReadLine()!);
+
+Console.WriteLine("");
+Console.WriteLine("Loading...");
+Console.WriteLine("");
+
+if (grade >= 7 && grade <= 10)
+{
+    Console.WriteLine($"{name} Situation: Green! You're approved!");
+}
+else if (grade >= 5)
+{
+    Console.WriteLine($"{name} Situation: Blue! Ok!");
+}
+else if (grade >=0 && grade <= 4)
+{
+    Console.WriteLine($"{name} Situation: Red! Bad notices...");
+}
+else
+{
+    Console.WriteLine("Error! Invalid Grade!");
+}
