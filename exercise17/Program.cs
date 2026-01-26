9bfcd051bc82c45ae9a63544e4d482da8a148f15
Felipe Ribeiro <felpscripter@gmail.com>
2026-01-26 15:55:13 -0300

    Feat: Learning While.

---
+++ exercise17/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Write("What is the passport? ");
+int passport = int.Parse(Console.ReadLine()!);
+
+while (passport != 0)
+{   
+    Console.WriteLine("Invalid passport");
+    Console.WriteLine($"The code typed is: {passport}");
+    Console.Write("What is the passport? ");
+    int newCode = int.Parse(Console.ReadLine()!);
+    passport = newCode;
+}
