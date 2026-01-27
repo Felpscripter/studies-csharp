2a5528ca5536ea50342c032a880a8339a871a764
Felipe Ribeiro <felpscripter@gmail.com>
2026-01-27 10:31:54 -0300

    Feat: Learning Basic Functions with void or return.

---
+++ exercise22/Program.cs
@@ -0,0 +1,26 @@
+﻿int intFunction(int a)
+{
+    return a;
+}
+
+int result = intFunction(1);
+Console.WriteLine(result);
+
+
+double doubleFunction(double x, double y)
+{
+    return x - y;
+}
+
+double result2 = doubleFunction(10, 9);
+Console.WriteLine(result2);
+
+
+void welcome()
+{
+    Console.WriteLine("Hello, Felipe! Welcome to your C# course.");
+}
+
+welcome();
+welcome();
+welcome();
