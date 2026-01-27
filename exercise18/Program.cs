ba2ce774040527fac678e98bd8166540841de1f7
Felipe <felpscripter@gmail.com>
2026-01-26 21:39:26 -0300

    Feat: Learning Do While.

---
+++ exercise18/Program.cs
@@ -0,0 +1,12 @@
+﻿int password = 0;
+
+do
+{
+    Console.WriteLine("What is the password? ");
+    int passwordTry = int.Parse(Console.ReadLine()!);
+    password = passwordTry;
+}
+
+while (password != 1234);
+
+Console.WriteLine("Acess granted!");
