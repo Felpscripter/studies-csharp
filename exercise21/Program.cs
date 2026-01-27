57bdf3edcffc7638fdc52c8d16bfbf8174bc8481
Felipe Ribeiro <felpscripter@gmail.com>
2026-01-27 09:35:29 -0300

    Feat: Learning break/continue.

---
+++ exercise21/Program.cs
@@ -0,0 +1,24 @@
+﻿for (int numbers =  1; numbers <= 20; numbers++)
+{
+    if (numbers == 13)
+    {
+        break;
+    }
+    else
+    {
+        Console.WriteLine(numbers);
+    }
+}
+
+
+for (int numbrs = 1; numbrs <= 20; numbrs++)
+{
+    if (numbrs % 4 == 0)
+    {
+        continue;
+    }
+    else
+    {
+        Console.WriteLine(numbrs);
+    }
+}
