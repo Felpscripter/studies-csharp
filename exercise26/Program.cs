a8aaced1cbe7e4e2a8f802cf2781ff71511f0b93
Felipe Ribeiro <felpscripter@gmail.com>
2026-01-29 10:05:11 -0300

    Feat: Arrays.

---
+++ exercise26/Program.cs
@@ -0,0 +1,22 @@
+﻿int[] numbers = {1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18};
+int numSome = 0;
+
+foreach (int num in numbers)
+{
+    Console.WriteLine(num);
+}
+
+foreach (int num2 in numbers)
+{
+    if (num2 % 2 == 0)
+    {
+        Console.WriteLine(num2);
+    }
+}
+
+foreach (int num3 in numbers)
+{
+    numSome = numSome + num3;
+}
+
+Console.WriteLine(numSome);
