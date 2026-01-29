5e915a6d0b5f1248403405ecce46b44e644e3014
Felipe Ribeiro <felpscripter@gmail.com>
2026-01-29 09:30:02 -0300

    Feat: Learning scopes (Proposital error).

---
+++ exercise25/Program.cs
@@ -0,0 +1,21 @@
+﻿void number(int myNumber)
+{
+    if (myNumber % 2 == 0)
+    {
+        int myNumberEven = 2;
+        Console.WriteLine("Even");
+        Console.WriteLine(myNumberEven);
+    }
+    else
+    {
+        Console.WriteLine("odd!");
+    }
+}
+
+Console.WriteLine(myNumberEven);
+number(1);
+number(2);
+number(3);
+number(4);
+number(5);
+number(6);
