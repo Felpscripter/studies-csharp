93c0efc04d802b9cbaf277516f91a6c001188eaa
Felipe Ribeiro <felpscripter@gmail.com>
2026-02-03 11:52:56 -0300

    Feat: Contains, Find, Any.

---
+++ exercise29/Program.cs
@@ -0,0 +1,9 @@
+﻿List<int> number = new List<int>{ 3, 7, 12, 18, 21, 25 };
+bool existNumber = number.Contains(18);
+Console.WriteLine(existNumber);
+
+int existNumber2 = number.Find(n => n > 10);
+Console.WriteLine(existNumber2);
+
+bool existEven = number.Any(n => n % 3 != 0);
+Console.WriteLine(existEven);
