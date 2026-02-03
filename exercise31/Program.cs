94371e13d7c8c3bcf353132b6eb9edb707dbb682
Felipe Ribeiro <felpscripter@gmail.com>
2026-02-03 12:56:39 -0300

    Feat: Sort, Reverse.

---
+++ exercise31/Program.cs
@@ -0,0 +1,15 @@
+﻿List<int> numbers = new List<int>{ 45, 2, 19, 8, 33, 10 };
+
+numbers.Sort();
+
+foreach (int x in numbers)
+{
+    Console.WriteLine(x);
+}
+
+numbers.Reverse();
+
+foreach (int y in numbers)
+{
+    Console.WriteLine(y);
+}
