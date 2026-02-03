6c8e103ee23c28f4dc23fc2d712bf1b2101e8cc4
Felipe Ribeiro <felpscripter@gmail.com>
2026-02-03 10:57:19 -0300

    Feat: learning List<>.

---
+++ exercise28/Program.cs
@@ -0,0 +1,24 @@
+﻿List<int> numbers = new List<int>();
+
+numbers.Add(5);
+
+foreach(int n in numbers)
+{
+    Console.WriteLine(n);
+}
+
+List<int> num = new List<int> {1,2,3,4,5,6,7,8,9,10};
+
+num.Remove(5);
+
+foreach(int n2 in num)
+{
+    Console.WriteLine(n2);
+}
+
+List<string> words = new List<string>{"Hello! ", "How are", "you?"};
+
+foreach(string w in words)
+{
+    Console.WriteLine(w);
+}
