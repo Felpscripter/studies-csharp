aaadbce825e2bba0b1dbd1013ea1b680961a8818
Felipe Ribeiro <felpscripter@gmail.com>
2026-02-03 14:40:15 -0300

    Feat: Queue.

---
+++ exercise33/Program.cs
@@ -0,0 +1,16 @@
+﻿Queue<string> queue = new Queue<string>();
+
+queue.Enqueue("John");
+queue.Enqueue("Maria");
+queue.Enqueue("Morgan");
+queue.Enqueue("Juliet");
+queue.Enqueue("Lourie");
+
+foreach(string x in queue)
+{
+    Console.WriteLine(x);
+}
+
+Console.WriteLine(queue.Dequeue());
+Console.WriteLine(queue.Dequeue());
+Console.WriteLine(queue.Peek());
