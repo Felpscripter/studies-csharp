9c4ff7dfa9b19cdf511a9056637a31989507ff25
Felipe Ribeiro <felpscripter@gmail.com>
2026-02-03 13:13:31 -0300

    Feat: Dictionary.

---
+++ exercise32/Program.cs
@@ -0,0 +1,12 @@
+﻿Dictionary<string, double> products = new Dictionary<string, double>();
+
+products["Notebook"] = 3500;
+products["Mouse"] = 120;
+products["Keyboard"] = 250;
+
+Console.WriteLine(products["Mouse"]);
+
+foreach (var prod in products)
+{
+    Console.WriteLine(prod);
+}
