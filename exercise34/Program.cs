923d3a5ead104a0631d0b4a56fc1257045980745
Felipe Ribeiro <felpscripter@gmail.com>
2026-02-05 08:32:34 -0300

    Feat: Stack.

---
+++ exercise34/Program.cs
@@ -0,0 +1,19 @@
+﻿Stack<int> numbers = new Stack<int>();
+
+numbers.Push(15);
+numbers.Push(3);
+numbers.Push(6);
+numbers.Push(9);
+numbers.Push(12);
+
+while (numbers.Count > 0)
+{
+    if (numbers.Peek() % 2 == 0)
+    {
+        Console.WriteLine(numbers.Pop());
+    }
+    else
+    {
+        break;
+    }
+}
